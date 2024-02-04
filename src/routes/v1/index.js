b9086a4ab7ff6e950334f36a42f775a5526f89c9
Surya Tanwar <forsurya02@gmail.com>
2024-02-04 16:19:00 +0530

    Started booking impl

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,10 @@ const express = require("express")
 const router = express.Router()
 
 const {InfoController} = require("../../controllers");
+const bookingRoutes = require('./booking');
 
 router.get("/info", InfoController.info);
+
+router.use('/bookings', bookingRoutes);
 
 module.exports = router
