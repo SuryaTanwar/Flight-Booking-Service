fb185144970126a912b4f4f0771020b38a4f3b72
Surya Tanwar <forsurya02@gmail.com>
2024-02-05 05:37:52 +0530

    Added cancel booking logic with cron

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { ServerConfig } = require("./config");
 
+const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes")
+const CRON = require("./utils/common/cron-jobs");
 
 const app = express();
 
@@ -12,4 +13,5 @@ app.use("/api", apiRoutes)
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
+    CRON();
 });
