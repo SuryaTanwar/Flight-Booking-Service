2bbb53ca87db832d8d6b70f3a2f09bdf9b57df2e
Surya Tanwar <forsurya02@gmail.com>
2024-02-07 09:00:10 +0530

    made proxy compliant

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use("/api", apiRoutes)
+app.use("/api", apiRoutes);
+//app.use("/bookingService/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
